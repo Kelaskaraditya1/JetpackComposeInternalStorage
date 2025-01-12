3496c645f0097da4f07822c1dfb7ccf0e7822de1
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2025-01-12 22:31:37 +0530

    Initial Commit

---
+++ app/src/main/java/com/starkindustries/jetpackcomposeinternalstorage/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.starkindustries.jetpackcomposeinternalstorage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.starkindustries.jetpackcomposeinternalstorage.ui.theme.JetpackComposeInternalStorageTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeInternalStorageTheme {
+
+            }
+        }
+    }
+}
+
